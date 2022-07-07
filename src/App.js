6049273e123d71bfaf681c2682785ee7ed581020
Park Dahyeon <dahyeon1712@inhandplus.com>
2022-07-07 20:00:48 +0900

    Signed-off-by: Park Dahyeon <dahyeon1712@inhandplus.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Button, Input, IconButton, Chip } from '@mui/material';
 import { useRef, forwardRef, useCallback } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -17,10 +18,28 @@ function App() {
 
   useEffect(() => {
     console.log(file)
+    //console.log(inputRef.current, '123')
   }, [file])
 
   const deleteFile = () => {
     setFile('')
+    inputRef.target.value = ''
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file.target.files[0]);
+    //formData.append("comment", commentValue);
+    //formData.append("content_id", classData.content_id);
+
+    axios({
+      method: "post",
+      url: 'http://localhost:2645/xlsx',
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" }
+    });
+
   }
 
 
@@ -35,12 +54,12 @@ function App() {
         </label>
         <Button variant="contained" alt='123123' onClick={inputTrigger} >Microsoft EXCEL 로 부터 가져오기</Button>
 
-        <div style={{"margin": "0 0 100px 0"}} />
+        <div style={{"margin": "0 0 60px 0"}} />
         {file && <Chip label={file.target.value} variant="outlined" onDelete={deleteFile} />}
 
-        <div style={{"margin": "0 0 100px 0"}} />
+        <div style={{"margin": "0 0 60px 0"}} />
         
-        {file ? <Button variant="contained">보내기</Button> : <Button variant="contained" disabled>보내기</Button>}
+        {file ? <Button variant="contained" onClick={handleSubmit}>보내기</Button> : <Button variant="contained" disabled>보내기</Button>}
         </div>
       </div>
     </div>
